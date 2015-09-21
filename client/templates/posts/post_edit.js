fe90d2667ca9619ad6aa5da8d4d928dd184da16f
cpachomski <cpachomski@gmail.com>
2015-09-21 14:02:07 -0400

    add session set and get for error messages and class for postEdit

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,18 @@
+Template.postEdit.created = function() {
+  Session.set('postEditErrors', {});
+}
+
+template.postEdit.helpers({
+  errorMessage: function(field){
+    return Session.get('psotEditErrors')[field];
+  },
+  errorClass: function(field){
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
+
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
